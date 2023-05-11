6d7e1d8ce8480d0fdf2970093cbefa2edae058f0
Reyner Veliz <reyner@reynerveliz.com>
2023-05-11 16:38:33 -0600

    Change form Color Scheme

--- skin_pro/Form1.vb
+++ skin_pro/Form1.vb
@@ -4,5 +4,10 @@
         sm = MaterialSkin.MaterialSkinManager.Instance
         sm.AddFormToManage(Me)
         sm.Theme = MaterialSkin.MaterialSkinManager.Themes.DARK
+        sm.ColorScheme = New MaterialSkin.ColorScheme(MaterialSkin.Primary.Orange500,
+                                                      MaterialSkin.Primary.Orange900,
+                                                      MaterialSkin.Primary.Orange200,
+                                                      MaterialSkin.Accent.Blue400,
+                                                      MaterialSkin.TextShade.WHITE)
     End Sub
 End Class
