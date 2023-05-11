d228b1ce4c79a00bf5605d683e5dacc5a8df58c2
Reyner Veliz <reyner@reynerveliz.com>
2023-05-11 16:16:58 -0600

    Add code to form for loadaing skin

--- skin_pro/Form1.vb
+++ skin_pro/Form1.vb
@@ -1,3 +1,7 @@
 ﻿Public Class Form1
-
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim sm As MaterialSkin.MaterialSkinManager
+        sm = MaterialSkin.MaterialSkinManager.Instance
+        sm.AddFormToManage(Me)
+    End Sub
 End Class
